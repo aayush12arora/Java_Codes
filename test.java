2af02bb0f3db0bc3c9d1a64a99ee09cd589a5b92
Aayush <aayush12arora@gmail.com>
2023-05-25 01:45:25 +0530

    first commit

---
+++ test.java
@@ -0,0 +1,14 @@
+package Java_Codes;
+
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+      
+
+        int a =0;
+        System.out.println(a);
+
+    }
+    
+}
